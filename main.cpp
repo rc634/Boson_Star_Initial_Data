b257d388346d7d50c271c74128f8749b1ae0445c
Robin Croft <rc634@cam.ac.uk>
2020-06-02 17:08:05 +0100

    started git repo

---
+++ main.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include "BosonStarParams.hpp"
+#include "BosonStarSolution.hpp"
+#include "BosonStarSolution.impl.hpp"
+#include "cmath"
+#include <vector>
+
+int main()
+{
+	double r_max = 500.;
+
+	BosonStarParams a_BosonStarParams;
+
+	BosonStarSolution a_boson_star;
+
+	a_boson_star.set_initialcondition_params(a_BosonStarParams, r_max);
+
+	a_boson_star.main();
+
+	return 0;
+}
