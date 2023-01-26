51afa4ddd9e6f0feedfb59e08e4c5bedf8c48fec
Robin Croft <robin@Robins-MacBook-Pro.local>
2023-01-26 14:54:42 +0100

    just an update

--- BosonStarParams.hpp
+++ BosonStarParams.hpp
@@ -1,10 +1,10 @@
 struct BosonStarParams
 	{
-		int gridpoints = 256001;
-		double central_amplitude_CSF = 0.0124; // 0.17  (ulis soliton) ;//0.050777063;//0.07683 (kaup limit) ;//0.0278 (old production run star);
+		int gridpoints = 128001; // 512001;
+		double central_amplitude_CSF = 0.03775; // 0.17  (ulis soliton) ;//0.050777063;//0.07683 (kaup limit) ;//0.0278 (old production run star);
 	    int eigen = 0;
 	    double scalar_mass = 1.;
 	    double phi4_coeff = 0.;
 	    bool solitonic = false;
-	    double sigma_soliton = 0.05;//0.0499;
+	    double sigma_soliton = 0.5;//0.0499;
 	};
--- main.cpp
+++ main.cpp
@@ -7,16 +7,22 @@
 
 int main()
 {
-	double r_max = 256.;
+	// maximum radius of solution, not stored in default params
+	double r_max = 128.;
 
-	BosonStarParams a_BosonStarParams;
+	// default parameters stored in BosonStarParams.hpp
+	BosonStarParams a_BosonStarParams; 
 
-	std::cout.precision(15);
+	// working precision
+	std::cout.precision(7); 
 
+	// mass and radius of star
 	double m, r;
 
+	// used to time program
 	double time_prev, time_new;
 
+    // files for saving data
 	std::ofstream mass_phi_file;//, mass_r_file;
 	//mass_r_file.open("mass_vs_r.csv");
 	mass_phi_file.open("mass_vs_phi.csv");
@@ -24,20 +30,33 @@ int main()
 	for (int i = 0; i < 1; ++i)
 	{
 
+		// Print out parameters to terminal - can ignore if desired
+		std::cout << "#############" << std::endl;
 		std::cout << "Iteration : " << i << std::endl;
 		time_prev = time(NULL);
 		std::cout << "Gridsize : " << a_BosonStarParams.gridpoints << ", L : " << r_max << std::endl;
+		std::cout << "#############" << std::endl;
+
+		// initialise boson star object
 		BosonStarSolution a_boson_star;
-                //a_BosonStarParams.central_amplitude_CSF = 0.03775;
+
+		// can set boson star params here if desired
+		// useful if you want to loop over different settings
+        //a_BosonStarParams.central_amplitude_CSF = 0.03775;
+
+
 		a_boson_star.set_initialcondition_params(a_BosonStarParams, r_max);
 		a_boson_star.main();
 		time_new = time(NULL);
 		std::cout << "Computation time : " << time_new - time_prev << " seconds" << std::endl << std::endl;
-		a_boson_star.output_csv();
+		//a_boson_star.output_csv();
 		m = a_boson_star.get_mass();
 		r = a_boson_star.get_r(0.01);
 		mass_phi_file << a_BosonStarParams.central_amplitude_CSF << ", " << m << ", " << std::endl;
 		//mass_r_file << r << ", " << m << ", " << std::endl;
+
+		// Setup the parameters for each star and printout to terminal 
+		std::cout << "#############" << std::endl;
 	}
 
 	mass_phi_file.close();
